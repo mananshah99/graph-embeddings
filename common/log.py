16acedddf398335489e5e5bac6b954d53bd93e1a
Manan Shah <manan.shah.777@gmail.com>
2017-06-06 09:52:28 -0700

    Tab width = 4 spaces, tabs -> spaces

--- common/log.py
+++ common/log.py
@@ -1,13 +1,13 @@
 import logging
 
 def default_logging(name):
-	# prints log to stdout and also saves to specified log file
-	logger = logging.getLogger(name)
-	fh = logging.FileHandler(name + '.log')
-	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-	fh.setFormatter(formatter)
-	ch = logging.StreamHandler()
-	ch.setFormatter(formatter)
-	logger.addHandler(fh)
-	logger.addHandler(ch)
-	logger.setLevel(logging.DEBUG)
+    # prints log to stdout and also saves to specified log file
+    logger = logging.getLogger(name)
+    fh = logging.FileHandler(name + '.log')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    logger.setLevel(logging.DEBUG)
