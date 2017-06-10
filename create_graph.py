b34a88dd5ffda41857ab648fe1b8c707566cfca8
Manan Shah <manan.shah.777@gmail.com>
2017-06-09 17:10:48 -0700

    Update to process all files and create edgelists

--- create_graph.py
+++ create_graph.py
@@ -23,16 +23,20 @@ def create_graph(name, weighted=False):
     
     nd = re.compile(r'[^\d.]+')
     for line in tqdm(i, total=util.count_lines(settings.INDIVIDUAL_PPI_DIR + '/' + name + '.txt')):
-        v = line.split(' ')
-        w = int(v[2]) + int(v[3]) + int(v[4]) + int(v[5]) + int(v[6]) + int(v[7])
-        n1 = int(nd.sub('', v[0].split('.')[1]).lstrip('0'))
-        n2 = int(nd.sub('', v[1].split('.')[1]).lstrip('0'))
+        try:
+            v = line.split(' ')
+            w = int(v[2]) + int(v[3]) + int(v[4]) + int(v[5]) + int(v[6]) + int(v[7])
+            n1 = int(nd.sub('', v[0].split('.')[1]).lstrip('0'))
+            n2 = int(nd.sub('', v[1].split('.')[1]).lstrip('0'))
     
-        if weighted and w > 0:
-            o.write(str(n1) + ' ' + str(n2) + ' ' + str(w) + '\n')
-        if not weighted and w > 0:
-            o.write(str(n1) + ' ' + str(n2) + '\n')
-
+            if weighted and w > 0:
+                o.write(str(n1) + ' ' + str(n2) + ' ' + str(w) + '\n')
+            if not weighted and w > 0:
+                o.write(str(n1) + ' ' + str(n2) + '\n')
+        except:
+            print "ERROR - ", line
+            pass
+    o.close()
 
 def visualize_graph(graph_path, output = ''):
     weighted = False
@@ -54,6 +58,11 @@ def visualize_graph(graph_path, output = ''):
         output = graph_path.split('/')[-1].strip('.txt') + '.dot'
     
     G.write(output)
+
+for f in tqdm(os.listdir(settings.INDIVIDUAL_PPI_DIR), total=2031):
+    n = f.split('/')[-1].split('.')[0]
+    create_graph(n, weighted=False)
+    create_graph(n, weighted=True)
 
 #create_graph('882', weighted=True)
 #visualize_graph(settings.INDIVIDUAL_W_GRAPH_DIR + '/882.txt')
