077c1ec1396cabcdce73f172bf6ea63141eaa301
Manan Shah <manan.shah.777@gmail.com>
2017-06-13 13:06:33 -0700

    Update settings filepaths

--- common/settings.py
+++ common/settings.py
@@ -4,8 +4,8 @@ common variables for file locations
 
 N2V_DIR = '/afs/cs.stanford.edu/u/manans/Desktop/snap/examples/node2vec/node2vec'
 
-PROTEIN_LINKS = '/dfs/ilfs2/0/MINER-BIO/miner-data/raw-data/STRING/STRING_v10/protein.links.full.v10.txt'
-SPECIES_MAPPING = '/dfs/ilfs2/0/MINER-BIO/miner-data/raw-data/STRING/STRING_v10/species.v10.txt'
+PROTEIN_LINKS = '/dfs/ilfs2/0/MINER-BIO/data-miner/raw-data/STRING/STRING_v10/protein.links.full.v10.txt' #'/dfs/ilfs2/0/MINER-BIO/miner-data/raw-data/STRING/STRING_v10/protein.links.full.v10.txt'
+SPECIES_MAPPING = '/dfs/ilfs2/0/MINER-BIO/data-miner/raw-data/STRING/STRING_v10/species.v10.txt'
 INDIVIDUAL_PPI_DIR = '/dfs/scratch0/manans/ppi'
 INDIVIDUAL_UW_GRAPH_DIR = '/dfs/scratch0/manans/ppi-uw-graph'
 INDIVIDUAL_W_GRAPH_DIR = '/dfs/scratch0/manans/ppi-w-graph'
