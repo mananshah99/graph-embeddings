7b1fb27c256f69f763d8a26dbe3e0644d9aa953a
Manan Shah <manan.shah.777@gmail.com>
2017-06-25 16:00:54 -0700

    Add COG mappings to path links

--- common/settings.py
+++ common/settings.py
@@ -6,6 +6,8 @@ N2V_DIR = '/afs/cs.stanford.edu/u/manans/Desktop/snap/examples/node2vec/node2vec
 
 PROTEIN_LINKS = '/dfs/ilfs2/0/MINER-BIO/data-miner/raw-data/STRING/STRING_v10/protein.links.full.v10.txt' #'/dfs/ilfs2/0/MINER-BIO/miner-data/raw-data/STRING/STRING_v10/protein.links.full.v10.txt'
 SPECIES_MAPPING = '/dfs/ilfs2/0/MINER-BIO/data-miner/raw-data/STRING/STRING_v10/species.v10.txt'
+COG_MAPPING = '/dfs/ilfs2/0/MINER-BIO/data-miner/raw-data/STRING/STRING_v10/COG.mappings.v10.txt'
+
 INDIVIDUAL_PPI_DIR = '/dfs/scratch0/manans/ppi'
 INDIVIDUAL_UW_GRAPH_DIR = '/dfs/scratch0/manans/ppi-uw-graph'
 INDIVIDUAL_W_GRAPH_DIR = '/dfs/scratch0/manans/ppi-w-graph'
