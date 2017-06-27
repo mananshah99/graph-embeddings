d862d86dcff51235e7804e66f661e3ef6eb83007
Manan Shah <manan.shah.777@gmail.com>
2017-06-27 15:33:54 -0700

    Include push notification handler

--- common/util.py
+++ common/util.py
@@ -8,3 +8,8 @@ def count_lines(file_path):
     while buf.readline():
         lines += 1
     return lines
+
+def push_notify(message):
+    m_str = '"message=' + str(message)
+    cmd = 'curl -s --form-string "token=aph11coe778qdkgm431sksmru3r38b" --form-string "user=ufsfrfpyq5oo633jetx71ky9tngabe" --form-string ' + m_str + '" https://api.pushover.net/1/messages.json > /dev/null'
+    os.system(cmd)
