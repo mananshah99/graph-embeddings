42e5dc2ef6deed321d706ee80e57f43579de80df
Manan Shah <manan.shah.777@gmail.com>
2017-08-09 11:15:15 -0700

    Update embedding evaluation (pt. 2)

--- embed/evaluate.py
+++ embed/evaluate.py
@@ -15,10 +15,20 @@ import util
 from embed import embed
 from gen_eval import GraphEvaluator
 
-e = GraphEvaluator(graph_type = 'full', nnodes = 10, k = 5, graph_directory = '/dfs/scratch0/manans/rewire-exp')
+def accuracy(tup):
+    return tup[0] / float(tup[0] + tup[1])
+
+print "#1 -> Creating Graphs"
+e = GraphEvaluator(graph_type = 'erdos-renyi', nnodes = 500, k = 10, graph_directory = '/dfs/scratch0/manans/rewire-exp')
 e.initialize()
 
-embeddings = embed('/dfs/scratch0/manans/rewire-exp', '/dfs/scratch0/manans/rewire-exp-emb',
-                    method = 'n2v', sample = -1)
+print "#2 -> Running Evaluation"
+for s in [100, 50, 10]:
 
-print e.evaluate(embeddings)
+    embeddings = embed('/dfs/scratch0/manans/rewire-exp', '/dfs/scratch0/manans/rewire-exp-emb',
+                    method = 'n2v', sample = s, verbose = False)
+
+    result = e.evaluate(embeddings, verbose = False)
+    print embeddings['0']
+    print s, result, accuracy(result)
+
