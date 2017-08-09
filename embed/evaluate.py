db35ed715ec8f3acdbf5a4df277d48e2fdb796da
Manan Shah <manan.shah.777@gmail.com>
2017-08-09 16:22:23 -0700

    Modularize evaluation code and streamline experiments

--- embed/evaluate.py
+++ embed/evaluate.py
@@ -14,21 +14,30 @@ import settings
 import util
 from embed import embed
 from gen_eval import GraphEvaluator
+import numpy as np
 
 def accuracy(tup):
     return tup[0] / float(tup[0] + tup[1])
 
+def run_x(fun):
+    vals = [accuracy(fun) for i in xrange(100)]
+    print vals
+
 print "#1 -> Creating Graphs"
-e = GraphEvaluator(graph_type = 'erdos-renyi', nnodes = 500, k = 10, graph_directory = '/dfs/scratch0/manans/rewire-exp')
-e.initialize()
+e = GraphEvaluator(graph_type = 'star', nnodes = 500, k = 10, graph_directory = '/dfs/scratch0/manans/rewire-exp')
+e.initialize(rewire_edge_percent = True)
 
 print "#2 -> Running Evaluation"
-for s in [100, 50, 10]:
+
+V_R = [accuracy(e.evaluate_random(verbose = False)) for i in xrange(100)]
+print "R =>", np.mean(V_R), np.median(V_R), np.std(V_R)
+
+''' Evaluation 1: Graph Type v Post-Rewiring Accuracy'''
+for s in [5, 10, 50, 100, 250, -1]:
 
     embeddings = embed('/dfs/scratch0/manans/rewire-exp', '/dfs/scratch0/manans/rewire-exp-emb',
                     method = 'n2v', sample = s, verbose = False)
 
     result = e.evaluate(embeddings, verbose = False)
-    print embeddings['0']
-    print s, result, accuracy(result)
-
+    V = [accuracy(e.evaluate(embeddings, verbose = False)) for i in xrange(100)]
+    print str(s) + " =>", np.mean(V), np.median(V), np.std(V)
