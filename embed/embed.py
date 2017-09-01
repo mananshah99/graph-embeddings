e32028d35eeaac5e3f1804bf0cc613880a887fa3
Manan Shah <manan.shah.777@gmail.com>
2017-09-01 14:58:19 -0700

    Update embedding script to include nf embeddings

--- embed/embed.py
+++ embed/embed.py
@@ -6,11 +6,13 @@ from tqdm import tqdm
 from os import listdir
 from os.path import isfile, join
 
-sys.path.insert(0, '/afs/cs.stanford.edu/u/manans/Desktop/graph-embeddings/embed-n2v')
-sys.path.insert(0, '/afs/cs.stanford.edu/u/manans/Desktop/graph-embeddings/common/')
+sys.path.insert(0, '/afs/cs.stanford.edu/u/manans/graph-embeddings/embed-n2v')
+sys.path.insert(0, '/afs/cs.stanford.edu/u/manans/graph-embeddings/common/')
+sys.path.insert(0, '/afs/cs.stanford.edu/u/manans/graph-embeddings/embed-nf/original/')
 import settings
 import util
 import embed_n2v as n2v
+import embed_nf_original as nfo
 
 def embed(input_directory,
           output_directory,
@@ -29,4 +31,9 @@ def embed(input_directory,
     if method == 'gcn':
         return -1 # not implemented yet, need to include training phase as well
 
+    if method == 'nf-o' or method == 'nf-original':
+        return nfo.embed_nf_original(input_directory, output_directory,
+                                    werbose = verbose, overwrite = overwrite)
 
+    if method == 'nf-k' or method == 'nf-keras':
+        return -1 # need to implement this 
