2056051fdd63af096b2b9cc2e2e472eba1ee871e
Manan Shah <manan.shah.777@gmail.com>
2017-09-03 22:06:29 -0700

    Merge evaluation scripts to centralized script

--- embed/evaluate.py
+++ embed/evaluate.py
@@ -24,7 +24,6 @@ import numpy as np
 from embed import embed
 from gen_eval import GraphEvaluator
 import datetime
-from tqdm import tqdm
 
 EXP_FILE = "exp_begin-aug10.csv"
 out = open(EXP_FILE, "a")
@@ -36,17 +35,21 @@ def metrics(l):
     return str(np.mean(l)), str(np.std(l))
 
 def run(graph_type, nnodes, f):
-    
-    # to be safe
     os.system('rm /dfs/scratch0/manans/rewire-exp-' + graph_type + '-emb/*')
 
-    e = GraphEvaluator(graph_type = graph_type, nnodes = nnodes, k = 50, graph_directory = '/dfs/scratch0/manans/rewire-exp-' + graph_type)
+    # every instance of 'run' operates on the same set of graphs, initialized
+    # by instance 'e' of GraphEvaluator
+    e = GraphEvaluator(graph_type = graph_type,
+                        nnodes = nnodes, 
+                        k = 100, 
+                        graph_directory = '/dfs/scratch0/manans/rewire-exp-' + graph_type)
     e.initialize()
+
     header = graph_type + '_k=' + str(50) + '_n=' + str(nnodes) + '_e=' + str(e.graph().GetEdges()) + '_t=' + datetime.datetime.now().isoformat().split('.')[0] + ','
-    
     means = " ," # first column has no associated values, just graph description
     stds  = " ,"
 
+    '''
     # method 1: random
     header += "random,"
     v = metrics([accuracy(e.evaluate_random(verbose = False)) for i in xrange(100)])
@@ -56,16 +59,32 @@ def run(graph_type, nnodes, f):
     # method 2-11: node2vec [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     for i in tqdm([10, 20, 30, 40, 50, 60, 70, 80, 90, -1]):
         header += ("n2v-0." + str(i) + "," if i > 0 else "n2v-1.0,")
-        embeddings = embed('/dfs/scratch0/manans/rewire-exp-' + graph_type,
-                            '/dfs/scratch0/manans/rewire-exp-' + graph_type + '-emb',
+
+        # only embed the test set (discard train)
+        embeddings = embed('/dfs/scratch0/manans/rewire-exp-' + graph_type + '/test',
+                            '/dfs/scratch0/manans/rewire-exp-' + graph_type + '-emb/test',
                             method = 'n2v', sample = int((float(i) / 100) * nnodes) if i > 0 else -1, verbose = False)
 
         v = metrics([accuracy(e.evaluate(embeddings, verbose = False)) for i in xrange(100)])
         means += v[0] + "," 
         stds  += v[1] + ","
 
-        # to be safe
-        os.system('rm /dfs/scratch0/manans/rewire-exp-' + graph_type + '-emb/*')
+        os.system('rm /dfs/scratch0/manans/rewire-exp-' + graph_type + '-emb/train/*')
+        os.system('rm /dfs/scratch0/manans/rewire-exp-' + graph_type + '-emb/test/*')
+
+    '''
+    
+    # method 12: nf-original
+
+    embeddings = embed('/dfs/scratch0/manans/rewire-exp-' + graph_type + '/train/',
+                        '/dfs/scratch0/manans/rewire-exp-' + graph_type + '/test/', 
+                        '/dfs/scratch0/manans/rewire-exp-' + graph_type + '-emb/test/',
+                        method = 'nf-original')
+
+
+    print embeddings
+    #TODO: Write method 13: hamilton graphSAGE with train and test directories
+
 
     # remove ending comma
     header = header[:-1]
@@ -76,7 +95,7 @@ def run(graph_type, nnodes, f):
     f.write(means + "\n")
     f.write(stds + "\n")
 
-for i in range(100, 500, 50): # i is nnodes
+for i in range(50, 500, 50): # i is nnodes
     run('er', i, out)
     run('ba', i, out)
 
