6356a430742a6b14a92925f720c47866df8d8652
Manan Shah <manan.shah.777@gmail.com>
2017-09-03 22:06:51 -0700

    Begin improving embed general function

--- embed/embed.py
+++ embed/embed.py
@@ -14,16 +14,23 @@ import util
 import embed_n2v as n2v
 import embed_nf_original as nfo
 
-def embed(input_directory,
-          output_directory,
-          method = 'n2v',
+def embed(train_input_directory,
+          test_input_directory,
+          test_output_directory,
+          method,
+
+          # node2vec
           sample = -1,
           selected_nodes = "",
+
+          # nf-original
+          weights = None,
+
           verbose = True,
           overwrite = True):
 
     if method == 'n2v': 
-        return n2v.embed_n2v(input_directory, output_directory,
+        return n2v.embed_n2v(test_input_directory, test_output_directory,
                         weighted = False, verbose = verbose,
                         sample = sample, selected_nodes = selected_nodes,
                         overwrite = overwrite)
@@ -32,8 +39,11 @@ def embed(input_directory,
         return -1 # not implemented yet, need to include training phase as well
 
     if method == 'nf-o' or method == 'nf-original':
-        return nfo.embed_nf_original(input_directory, output_directory,
-                                    werbose = verbose, overwrite = overwrite)
+
+        nfo.train_nf_original(train_input_directory)
+
+        return nfo.embed_nf_original(test_input_directory, test_output_directory,
+                                    verbose = verbose, overwrite = overwrite)
 
     if method == 'nf-k' or method == 'nf-keras':
         return -1 # need to implement this 
