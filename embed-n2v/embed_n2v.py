54fcfb3046372474d08e7616bd2914d3688bd4b9
Manan Shah <manan.shah.777@gmail.com>
2017-09-07 12:02:12 -0700

    Update embed_n2v.py

--- embed-n2v/embed_n2v.py
+++ embed-n2v/embed_n2v.py
@@ -6,7 +6,7 @@ import sys
 import subprocess
 from tqdm import tqdm
 
-sys.path.insert(0, '/afs/cs.stanford.edu/u/manans/graph-embeddings/common/')
+sys.path.insert(0, '/afs/cs.stanford.edu/u/manans/Desktop/graph-embeddings/common/')
 import settings
 import util
 import numpy as np
